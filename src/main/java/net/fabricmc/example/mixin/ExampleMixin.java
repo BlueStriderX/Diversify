e678c74a7d5cadb0e48cc6a85af57de54ac6a651
NZSmartie <nzsmartie@gmail.com>
2019-03-25 00:34:45 +1300

    Changes for StarMade

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,15 +1,21 @@
 package net.fabricmc.example.mixin;
 
-import net.minecraft.client.MinecraftClient;
+import org.schema.game.client.controller.GameClientController;
+import org.schema.schine.network.client.ClientController;
+import org.schema.schine.network.client.ClientState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftClient.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
+@Mixin(GameClientController.class)
+public abstract class ExampleMixin extends ClientController {
+	private ExampleMixin(ClientState clientState) {
+		super(clientState);
+	}
+
+	@Inject(at = @At("HEAD"), method = "initialize()V")
+	public void init(CallbackInfo info) {
 		System.out.println("This line is printed by an example mod mixin!");
 	}
 }
