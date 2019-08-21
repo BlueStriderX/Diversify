a8948dae4450a81780fa580510816c15f7805e86
TheDerpGamer <garret2kool@gmail.com>
2019-08-20 19:52:06 -0700

    Create Main.java

---
+++ src/net/thederpgamer/diversify/main/Main.java
@@ -0,0 +1,11 @@
+package net.thederpgamer.diversify.main;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+		System.out.println("Diversify Loaded!");
+	}
+}
